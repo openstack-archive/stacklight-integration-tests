1a221f52c38930546f95a4deed919c1ec2e27eac
Rodion Promyshlennikov <rpromyshlennikov@mirantis.com>
2016-04-28 15:34:04 +0300

    Create base structure for tests
    Add tox pep8 and hacking check.
    Add basic tests starter.
    
    Change-Id: I9553606250674e4ae0c5d6aca5d88b7a1fe39977

---
+++ utils/fuel-qa-builder/prepare_env.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+pip install -U pip || true
+
+VIRTUALENV_EXIST=`dpkg -l | grep python-virtualenv && pip list | grep virtualenv`
+
+if [[ -z "VIRTUALENV_EXIST" ]]; then
+    echo 'There is no virtualnev'
+    pip install virtualenv || apt-get install python-virtualenv || true
+fi
+
+if [ -z "${VENV_PATH}" ]; then
+    VENV_PATH="${BASE_DIR}"/venv-stacklight-tests
+fi
+
+virtualenv "${VENV_PATH}" || true
+
+. "${VENV_PATH}"/bin/activate
+
+
+
+mkdir tmp && cd tmp
+
+git clone https://github.com/openstack/fuel-qa.git && cd fuel-qa && git checkout stable/8.0 || true
+
+cp "${BASE_DIR}"/MANIFEST.in  ./ && cp "${BASE_DIR}"/setup.py ./
+
+python setup.py sdist && pip install dist/fuelweb_test*.tar.gz && pip install -r "${BASE_DIR}"/../../requirements.txt
+
+cd "${BASE_DIR}" && rm -rf tmp
---
+++ utils/fuel-qa-builder/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def get_requirements_list(requirements):
+    all_requirements = read(requirements)
+    all_requirements = [req for req in all_requirements.splitlines()
+                        if 'devops' not in req]
+    return all_requirements
+
+
+setup(
+    name='fuelweb_test',
+    version=8.0,
+    description='Fuel-qa fuelweb package',
+
+    url='http://www.openstack.org/',
+    author='OpenStack',
+    author_email='openstack-dev@lists.openstack.org',
+    packages=['fuelweb_test', 'gates_tests'],
+    include_package_data=True,
+    classifiers=[
+        'Environment :: Linux',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    install_requires=get_requirements_list('./fuelweb_test/requirements.txt'),
+)
