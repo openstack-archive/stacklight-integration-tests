3fedd401c8826a1bf83d117b6bc63e62a306dcad
Vladimir Ushakov <vushakov@mera.ru>
2016-05-11 14:44:02 +0300

    Add base testcase class suite.
    Add smoke bvt tests for lma infrastructure
    alerting plugin
    Update common helpers
    
    Change-Id: I55f50bfc78cca2a116ff208fa4b1ce19453a42f5

---
+++ stacklight_tests/lma_infrastructure_alerting/plugin_settings.py
@@ -0,0 +1,34 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from stacklight_tests import settings
+
+name = 'lma_infrastructure_alerting'
+version = '0.9.0'
+role_name = ['infrastructure_alerting']
+vip_name = 'infrastructure_alerting_mgmt_vip'
+plugin_path = settings.LMA_INFRA_ALERTING_PLUGIN_PATH
+
+nagios_user = 'nagiosadmin'
+nagios_password = 'r00tme'
+send_to = 'root@localhost'
+send_from = 'nagios@localhost'
+smtp_host = '127.0.0.1'
+
+options = {
+    'nagios_password/value': nagios_password,
+    'send_to/value': send_to,
+    'send_from/value': send_from,
+    'smtp_host/value': smtp_host,
+}
