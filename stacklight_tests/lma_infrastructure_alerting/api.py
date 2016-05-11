603aed06ac2f485c7e3a113b39d4630411eba6d9
Vladimir Ushakov <vushakov@mera.ru>
2016-05-11 14:44:02 +0300

    Add base testcase class suite.
    Add smoke bvt tests for lma infrastructure
    alerting plugin
    Update common helpers
    
    Change-Id: I55f50bfc78cca2a116ff208fa4b1ce19453a42f5

---
+++ stacklight_tests/lma_infrastructure_alerting/api.py
@@ -0,0 +1,49 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from fuelweb_test import logger
+
+from stacklight_tests import base_test
+from stacklight_tests.lma_infrastructure_alerting import(
+    plugin_settings as infra_alerting_plugin_settings)
+
+
+class InfraAlertingPluginApi(base_test.PluginApi):
+    def get_plugin_settings(self):
+        return infra_alerting_plugin_settings
+
+    def prepare_plugin(self):
+        self.helpers.prepare_plugin(self.settings.plugin_path)
+
+    def activate_plugin(self):
+        self.helpers.activate_plugin(
+            self.settings.name, self.settings.version, self.settings.options)
+
+    def get_plugin_vip(self):
+        return self.helpers.get_plugin_vip(self.settings.vip_name)
+
+    def check_plugin_online(self):
+        lma_alerting_vip = self.get_plugin_vip()
+
+        logger.info("Check that the Nagios server is running")
+        self.checkers.check_http_get_response(
+            "http://{0}:{1}@{2}:8001".format(
+                self.settings.nagios_user, self.settings.nagios_password,
+                lma_alerting_vip))
+
+
+
+
+
+
