a73eadf7b08526b72087353089224f394d9115f5
Simon Pasquier <spasquier@mirantis.com>
2016-05-16 16:58:33 +0200

    Update prepare_env.sh
    This change rewrites a bit the prepare_env.sh script to make the
    installation process smoother.
    
    Change-Id: I486aa78888316548b9e8efdc1775c0b41d6532f0

--- utils/fuel-qa-builder/prepare_env.sh
+++ utils/fuel-qa-builder/prepare_env.sh
@@ -1,32 +1,48 @@
 #!/bin/bash
+#
+# Script to setup a Python virtual environment with all the dependencies
+# installed
 
+set -e
+
+# Initialize variables
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+VENV_PATH=${VENV_PATH:-"${BASE_DIR}"/venv-stacklight-tests}
+FUELQA_GITREF=${FUELQA_GITREF:-stable/8.0}
+VIRTUALENV_BINARY=$(which virtualenv)
 
-pip install -U pip || true
-
-VIRTUALENV_EXIST=`dpkg -l | grep python-virtualenv || pip list | grep virtualenv`
-
-if [[ -z "${VIRTUALENV_EXIST}" ]]; then
-    echo 'There is no virtualnev'
-    pip install virtualenv || apt-get install python-virtualenv || true
+if [[ -z "${VIRTUALENV_BINARY}" ]]; then
+    echo 'Cannot find the virtualenv executable!'
+    echo 'You should install it either using pip or you distribution package manager.'
+    exit 1
 fi
 
-if [ -z "${VENV_PATH}" ]; then
-    VENV_PATH="${BASE_DIR}"/venv-stacklight-tests
+if [ ! -x "$VENV_PATH"/bin/activate ]; then
+    "$VIRTUALENV_BINARY" "$VENV_PATH"
 fi
 
-virtualenv "${VENV_PATH}" || true
+. "$VENV_PATH"/bin/activate
 
-. "${VENV_PATH}"/bin/activate
+# Always upgrade to the latest version of pip
+pip install -U pip
 
+# Install fuel-qa in the virtual environment
+FUELQA_GITREF=${FUELQA_GITREF:-stable/8.0}
+echo "Checking out fuel-qa/$FUELQA_GITREF"
+FUELQA_DIR=$(mktemp -d)
+git clone https://github.com/openstack/fuel-qa.git -- "$FUELQA_DIR"
 
+pushd "$FUELQA_DIR"
+git checkout "$FUELQA_GITREF"
 
-mkdir tmp && cd tmp
+cp "${BASE_DIR}"/{MANIFEST.in,setup.py} ./
 
-git clone https://github.com/openstack/fuel-qa.git && cd fuel-qa && git checkout stable/8.0 || true
+python setup.py sdist
+pip install dist/fuelweb_test*.tar.gz
 
-cp "${BASE_DIR}"/MANIFEST.in  ./ && cp "${BASE_DIR}"/setup.py ./
+# Clean up stuff
+popd
+rm -rf "$FUELQA_DIR"
 
-python setup.py sdist && pip install dist/fuelweb_test*.tar.gz && pip install -r "${BASE_DIR}"/../../requirements.txt
-
-cd "${BASE_DIR}" && rm -rf tmp
+# Install project's dependencies
+pip install -rrequirements.txt
