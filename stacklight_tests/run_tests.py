0c06f3fe23eb7e385212283f28807fce4d85240a
vgusev <vgusev@mirantis.com>
2016-05-18 18:31:35 +0300

    Add bvt tests for toolchain
    Change-Id: Ia307466a4ff177038d34829370d86f5db20a9f05

--- stacklight_tests/run_tests.py
+++ stacklight_tests/run_tests.py
@@ -47,9 +47,9 @@ def import_tests():
     from stacklight_tests.influxdb_grafana import test_smoke_bvt  # noqa
     from stacklight_tests.influxdb_grafana import test_system  # noqa
     from stacklight_tests.lma_collector import test_smoke_bvt  # noqa
-
     from stacklight_tests.lma_infrastructure_alerting import (  # noqa
         test_smoke_bvt)  # noqa
+    from stacklight_tests.toolchain import test_smoke_bvt  # noqa
 
 
 def run_tests():
