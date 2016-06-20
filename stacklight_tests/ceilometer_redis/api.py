5b2281ce3a0c79aebf8408dbf2a54574c7c09ce8
Artem <aminasyan@mirantis.com>
2016-06-20 17:21:36 +0300

    Add system and smoke test for Redis plugin
    Change-Id: Iff2a9a27919f4757edfd289260a48df260a3f110

---
+++ stacklight_tests/ceilometer_redis/api.py
@@ -0,0 +1,47 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from proboscis import asserts
+from stacklight_tests import base_test
+from stacklight_tests.ceilometer_redis import plugin_settings
+
+
+class CeilometerRedisPluginApi(base_test.PluginApi):
+    def get_plugin_settings(self):
+        return plugin_settings
+
+    def prepare_plugin(self):
+        self.helpers.prepare_plugin(self.settings.plugin_path)
+
+    def activate_plugin(self, options=None):
+        if options is None:
+            options = self.settings.default_options
+        self.helpers.activate_plugin(
+            self.settings.name, self.settings.version, options)
+
+    def check_plugin_online(self):
+        controller = self.fuel_web.get_nailgun_cluster_nodes_by_roles(
+            self.helpers.cluster_id, ['controller'])[0]
+        with controller.ssh() as remote:
+            cmd = remote.execute("pcs resource| grep 'Stopped'")['stdout']
+        msg = 'Some of the services stopped'
+        asserts.assert_true(len(cmd) > 0, msg)
+
+    def uninstall_plugin(self):
+        return self.helpers.uninstall_plugin(self.settings.name,
+                                             self.settings.version)
+
+    def check_uninstall_failure(self):
+        return self.helpers.check_plugin_cannot_be_uninstalled(
+            self.settings.name, self.settings.version)
