0ac5c3acb9a0b2ca0bfc0615229ea7e629f3276e
Artem <aminasyan@mirantis.com>
2016-07-05 14:05:22 +0300

    Add functional tests for Redis plugin
    Change-Id: Ief4112a01147a1859659a4370588f1d583cb34b8

--- stacklight_tests/run_tests.py
+++ stacklight_tests/run_tests.py
@@ -39,6 +39,7 @@ class CloseSSHConnectionsPlugin(plugins.Plugin):
 
 
 def import_tests():
+    from stacklight_tests.ceilometer_redis import test_functional  # noqa
     from stacklight_tests.ceilometer_redis import test_smoke_bvt  # noqa
     from stacklight_tests.ceilometer_redis import test_system  # noqa
     from stacklight_tests.elasticsearch_kibana import test_smoke_bvt  # noqa
