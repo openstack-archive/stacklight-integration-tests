ff2dd838d3a6179af4b9f5b6ddc75b1fae6b840f
Artem <aminasyan@mirantis.com>
2016-07-05 14:05:22 +0300

    Add functional tests for Redis plugin
    Change-Id: Ief4112a01147a1859659a4370588f1d583cb34b8

--- stacklight_tests/ceilometer_redis/api.py
+++ stacklight_tests/ceilometer_redis/api.py
@@ -11,12 +11,33 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-
+import ceilometerclient.v2.client
+from proboscis.asserts import assert_equal
 from stacklight_tests import base_test
 from stacklight_tests.ceilometer_redis import plugin_settings
+from stacklight_tests.helpers.helpers import NotFound
 
 
 class CeilometerRedisPluginApi(base_test.PluginApi):
+    def __init__(self):
+        super(CeilometerRedisPluginApi, self).__init__()
+        self._ceilometer = None
+
+    @property
+    def ceilometer(self):
+        if self._ceilometer is None:
+            keystone = self.helpers.os_conn.keystone
+            try:
+                endpoint = keystone.service_catalog.url_for(
+                    service_type='metering',
+                    endpoint_type='internalURL')
+            except NotFound("Cannot initialize ceilometer client"):
+                return None
+
+            self._ceilometer = ceilometerclient.v2.Client(
+                endpoint=endpoint, token=lambda: keystone.auth_token)
+        return self._ceilometer
+
     def get_plugin_vip(self):
         pass
 
@@ -26,9 +47,64 @@ class CeilometerRedisPluginApi(base_test.PluginApi):
     def prepare_plugin(self):
         self.helpers.prepare_plugin(self.settings.plugin_path)
 
-    def run_ostf(self):
-        self.helpers.run_ostf(test_sets=['sanity', 'smoke', 'ha',
-                                         'tests_platform'])
+    def run_ostf(self, skip_tests=None):
+        self.fuel_web.run_ostf(
+            cluster_id=self.helpers.cluster_id,
+            test_sets=['smoke', 'sanity'],
+            timeout=60 * 15
+        )
+
+        test_class_main = ('fuel_health.tests.tests_platform.'
+                           'test_ceilometer.'
+                           'CeilometerApiPlatformTests')
+        tests_names = ['test_check_alarm_state',
+                       'test_create_sample',
+                       'test_check_volume_events',
+                       'test_check_glance_notifications',
+                       'test_check_keystone_notifications',
+                       'test_check_neutron_notifications',
+                       'test_check_sahara_notifications',
+                       'test_check_events_and_traits']
+
+        test_classes = []
+
+        for test_name in tests_names:
+            test_classes.append('{0}.{1}'.format(test_class_main,
+                                                 test_name))
+
+        all_tests = [test['id'] for test
+                     in self.fuel_web.client.get_ostf_tests(self.helpers
+                                                            .cluster_id)]
+
+        for test_id in test_classes:
+            if test_id in all_tests:
+                if skip_tests and test_id.split('.')[-1] in skip_tests:
+
+                    all_status = self.fuel_web.run_single_ostf_test(
+                        cluster_id=self.helpers.cluster_id,
+                        test_sets=['tests_platform'],
+                        test_name=test_id, retries=True, timeout=60 * 20)
+
+                    test_name = next(
+                        test['name'] for test
+                        in self.fuel_web.client.get_ostf_tests(self.
+                                                               helpers.
+                                                               cluster_id)
+                        if test['id'] == test_id)
+
+                    status = next(test.values()[0]
+                                  for test in all_status
+                                  if test.keys()[0] == test_name)
+
+                    assert_equal(
+                        status, "skipped",
+                        'Test: "{}" must be skipped status, '
+                        'but his status {}'.format(test_name, status))
+                else:
+                    self.fuel_web.run_single_ostf_test(
+                        cluster_id=self.helpers.cluster_id,
+                        test_sets=['tests_platform'],
+                        test_name=test_id, timeout=60 * 20)
 
     def activate_plugin(self, options=None):
         if options is None:
---
+++ stacklight_tests/ceilometer_redis/test_functional.py
@@ -0,0 +1,123 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+from datetime import datetime
+from datetime import timedelta
+from fuelweb_test.helpers.decorators import log_snapshot_after_test
+from proboscis import asserts
+from proboscis import test
+from stacklight_tests.ceilometer_redis import api
+import tempfile
+import time
+
+
+@test(groups=["plugins"])
+class TestFunctionalCeilometerRedisPlugin(api.CeilometerRedisPluginApi):
+    """Class for functional testing of Ceilometer-Redis plugin."""
+
+    @test(depends_on_groups=["deploy_ceilometer_redis"],
+          groups=["check_plugin_services", "ceilometer_redis",
+                  "functional"])
+    @log_snapshot_after_test
+    def check_plugin_services(self):
+        """Verify that the plugin services are running"""
+
+        controller = self.fuel_web.get_nailgun_cluster_nodes_by_roles(
+            self.helpers.cluster_id, ['controller'])[0]
+        with self.fuel_web.get_ssh_for_nailgun_node(controller) as remote:
+            cmd = remote.execute("pcs resource | grep Stopped")['stdout']
+        msg = 'Some of the services stopped'
+        asserts.assert_true(len(cmd) == 0, msg)
+
+    @test(depends_on_groups=["deploy_ceilometer_redis"],
+          groups=["central_agents_restart", "ceilometer_redis",
+                  "functional"])
+    @log_snapshot_after_test
+    def central_agents_restart(self):
+        """Verify that all measurements are collected
+        after stopping one of central agents.
+        """
+
+        controller = self.fuel_web.get_nailgun_cluster_nodes_by_roles(
+            self.helpers.cluster_id, ['controller'])[0]
+        with self.fuel_web.get_ssh_for_nailgun_node(controller) as remote:
+            remote.execute('pcs resource ban p_ceilometer-agent-central '
+                           '$(hostname) --wait=100')
+            cmd = remote.execute(' ps aux | grep -v grep | grep'
+                                 ' -c ceilometer-polling')['stdout'][0]
+            msg = "Error, agent central was't stopped"
+            asserts.assert_true(int(cmd) == 0, msg)
+            self.run_ostf()
+
+    @test(depends_on_groups=["deploy_ceilometer_redis"],
+          groups=["check_polling_logs", "ceilometer_redis",
+                  "functional"])
+    @log_snapshot_after_test
+    def check_polling_logs(self):
+        """Check 'Joined partitioning group agent-central
+        in the logs of central agent
+        """
+
+        controller = self.fuel_web.get_nailgun_cluster_nodes_by_roles(
+            self.helpers.cluster_id, ['controller'])[0]
+        with self.fuel_web.get_ssh_for_nailgun_node(controller) as remote:
+            cmd = remote.execute("cat /var/log/ceilometer/"
+                                 "ceilometer-polling.log | "
+                                 "grep 'Joined partitioning "
+                                 "group central-global' ")['stdout']
+        msg = 'Agent central has some problem.Please,' \
+              'look logs for more details'
+        asserts.assert_true(len(cmd) > 0, msg)
+
+    @test(depends_on_groups=["deploy_ceilometer_redis"],
+          groups=["check_alarm_evaluator_logs", "ceilometer_redis",
+                  "functional"])
+    @log_snapshot_after_test
+    def check_alarm_evaluator_logs(self):
+        """Check 'Joined partitioning group alarm_evaluator'
+        in the logs of alarm evaluator.
+        """
+
+        controller = self.fuel_web.get_nailgun_cluster_nodes_by_roles(
+            self.helpers.cluster_id, ['controller'])[0]
+        with self.fuel_web.get_ssh_for_nailgun_node(controller) as remote:
+            cmd = remote.execute("cat /var/log/aodh/aodh-evaluator.log "
+                                 "| grep 'Joined partitioning group"
+                                 " alarm_evaluator' ")['stdout']
+        msg = 'Alarm evaluator has some problem.Please,' \
+              'look logs for more details'
+        asserts.assert_true(len(cmd) > 0, msg)
+
+    @test(depends_on_groups=["deploy_ceilometer_redis"],
+          groups=["check_samples", "ceilometer_redis",
+                  "functional"])
+    @log_snapshot_after_test
+    def check_samples(self):
+        """Check that for one polling interval only one 'image' sample exists.
+        """
+
+        with tempfile.TemporaryFile() as fp:
+            fp.write('Test')
+            fp.seek(0)
+            image = self.helpers.os_conn.create_image(name='Redis',
+                                                      container_format='bare',
+                                                      disk_format='qcow2',
+                                                      data=fp)
+        time.sleep(2 * self.settings.polling_interval)
+        f = datetime.now() - timedelta(seconds=600)
+        query = [{'field': 'timestamp', 'op': 'ge', 'value': f.isoformat()},
+                 {'field': 'resource_id', 'op': 'eq', 'value': image.id}]
+        lis = self.ceilometer.statistics.list(q=query,
+                                              meter_name='image')[0].count
+        msg = "Error, Excpected 1 image sample, got : {0} .".format(lis)
+        asserts.assert_true(lis == 1, msg)
