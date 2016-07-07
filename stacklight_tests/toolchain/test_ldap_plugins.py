04069e60637cab37a593b16a82e0fe9e76d3cf12
Igor Degtiarov <idegtiarov@mirantis.com>
2016-07-07 21:42:19 +0300

    Add LDAP test to toolchain
    scenario with authorization
    
    Change-Id: Ic5605439a94779e11eec21057c4a8581b6ec6198

--- stacklight_tests/toolchain/test_ldap_plugins.py
+++ stacklight_tests/toolchain/test_ldap_plugins.py
@@ -83,6 +83,66 @@ class TestToolchainLDAP(api.ToolchainApi):
 
         self.env.make_snapshot("deploy_toolchain_with_ldap", is_make=True)
 
+    @test(depends_on_groups=['prepare_slaves_3'],
+          groups=["install_toolchain", "install", "toolchain", "smoke"])
+    @log_snapshot_after_test
+    def deploy_toolchain_with_ldap_authz(self):
+        """Install the LMA Toolchain plugins and check exists and available
+
+        Scenario:
+            1. Upload the LMA Toolchain plugins to the master node
+            2. Install the plugins
+            3. Create the cluster
+            4. Enable and configure LDAP for plugin authentication and
+               authorization
+            5. Deploy the cluster
+            6. Upload install_slapd.sh script on plugin node
+            7. Install and configure the LDAP plugin
+            8. Check that LMA Toolchain plugins are running
+            9. Check plugins are available with LDAP for authentication and
+               authorization
+
+        Duration 120m
+        """
+        fuel_web = self.helpers.fuel_web
+
+        self.env.revert_snapshot("ready_with_3_slaves")
+
+        self.prepare_plugins()
+
+        self.helpers.create_cluster(name=self.__class__.__name__)
+
+        self.activate_plugins()
+
+        plugins_ldap = {
+            "kibana": self.ELASTICSEARCH_KIBANA,
+            "grafana": self.INFLUXDB_GRAFANA,
+            "nagios": self.LMA_INFRASTRUCTURE_ALERTING,
+        }
+
+        for name, plugin in six.iteritems(plugins_ldap):
+            self._activate_ldap_plugin(name, plugin, authZ=True)
+
+        self.helpers.deploy_cluster(self.settings.base_nodes)
+
+        plugin_node = fuel_web.get_nailgun_cluster_nodes_by_roles(
+            self.helpers.cluster_id, roles=["influxdb_grafana", ])[0]
+
+        with fuel_web.get_ssh_for_nailgun_node(plugin_node) as remote:
+            remote.upload(
+                helpers.get_fixture("ldap/install_slapd.sh"),
+                "/tmp"
+            )
+            remote.check_call(
+                "bash -x /tmp/install_slapd.sh", verbose=True
+            )
+
+        self.check_plugins_online()
+
+        self.check_plugins_ldap(authZ=True)
+
+        self.env.make_snapshot("deploy_toolchain_ldap_authZ", is_make=True)
+
     @staticmethod
     def _activate_ldap_plugin(name, plugin, authZ=False, protocol="ldap"):
         """Activate LDAP option in plugin."""
@@ -101,5 +161,29 @@ class TestToolchainLDAP(api.ToolchainApi):
                 "(uid=%s)" if name == "grafana" else "(objectClass=*)",
             "ldap_user_search_base_dns/value": "dc=stacklight,dc=ci",
         }
+
+        if authZ:
+            options.update({
+                "ldap_authorization_enabled/value": True,
+            })
+            if name in ["kibana", "nagios"]:
+                options.update({
+                    "ldap_admin_group_dn":
+                        "cn=plugin_admins,ou=groups,dc=stacklight,dc=ci"
+                })
+                if name == "kibana":
+                    options.update({
+                        "ldap_viewer_group_dn":
+                            "cn=plugin_viewers,ou=groups,dc=stacklight,dc=ci"
+                    })
+            else:
+                options.update({
+                    "ldap_group_search_base_dns/value":
+                        "ou=groups,dc=stacklight,dc=ci",
+                    "ldap_group_search_filter/value":
+                        "(&(objectClass=posixGroup)(memberUid=%s)",
+                    "ldap_admin_group_dn": "plugin_admins",
+                    "ldap_viewer_group_dn": "plugin_viewers"
+                })
 
         plugin.activate_plugin(options=options)
