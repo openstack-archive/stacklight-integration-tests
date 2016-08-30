17d7d4996738a50541186b8b9c52cbe1b513353f
Vladimir Ushakov <vushakov@mera.ru>
2016-08-30 10:55:47 +0300

    Add tests for rabbitmq alarms.
    Add tests for rabbitmq-disk-limit alarms.
    Add tests for rabbitmq-memory-limit alarms.
    
    Change-Id: Ia1e178cbeace91e978444cd44a5e0dcf0c08d7fd

--- stacklight_tests/toolchain/test_alarms.py
+++ stacklight_tests/toolchain/test_alarms.py
@@ -23,6 +23,10 @@ WARNING_STATUS = 1
 CRITICAL_STATUS = 3
 WARNING_PERCENT = 91
 CRITICAL_PERCENT = 96
+DISK_WARNING_PERCENT = 99.99
+DISK_CRITICAL_PERCENT = 100
+MEMORY_WARNING_VALUE = 1.01
+MEMORY_CRITICAL_VALUE = 1.0001
 
 
 @test(groups=["plugins"])
@@ -52,6 +56,47 @@ class TestToolchainAlarms(api.ToolchainApi):
             self.check_alarms(
                 alarm_type, source, nailgun_node["hostname"], OKAY_STATUS)
 
+    def _check_rabbit_mq_alarms(self, controller, alarm_type, source):
+        cmd = ("rabbitmqctl set_disk_free_limit $(df | grep /dev/dm-4 | "
+               "awk '{{ printf(\"%.0f\\n\", 1024 * ((($3 + $4) * "
+               "{percent} / 100) - $3))}}')")
+        self.check_alarms(alarm_type, source, controller["hostname"],
+                          OKAY_STATUS)
+        with self.fuel_web.get_ssh_for_nailgun_node(controller) as remote:
+            default_value = remote.check_call(
+                "rabbitmqctl environment | grep disk_free_limit | "
+                "sed -r 's/}.+//' | sed 's|.*,||'")['stdout'][0].rstrip()
+            remote.check_call(cmd.format(percent=DISK_WARNING_PERCENT))
+            self.check_alarms(alarm_type, source, controller["hostname"],
+                              WARNING_STATUS)
+            remote.check_call(cmd.format(percent=DISK_CRITICAL_PERCENT))
+            self.check_alarms(alarm_type, source, controller["hostname"],
+                              CRITICAL_STATUS)
+            remote.check_call("rabbitmqctl set_disk_free_limit {}".format(
+                default_value))
+            self.check_alarms(alarm_type, source, controller["hostname"],
+                              OKAY_STATUS)
+
+    def _check_rabbit_mq_memory_alarms(self, controller, alarm_type, source):
+        cmd = "rabbitmqctl set_vm_memory_high_watermark absolute \"{memory}\""
+        self.check_alarms(alarm_type, source, controller["hostname"],
+                          OKAY_STATUS)
+        with self.fuel_web.get_ssh_for_nailgun_node(controller) as remote:
+            default_value = remote.check_call(
+                "rabbitmqctl environment | grep disk_free_limit | "
+                "sed -r 's/}.+//' | sed 's|.*,||'")['stdout'][0].rstrip()
+            mem_usage = self.get_rabbitmq_memory_usage()
+            remote.check_call(cmd.format(memory=int(mem_usage * 1.01)))
+            self.check_alarms(alarm_type, source, controller["hostname"],
+                              WARNING_STATUS)
+            mem_usage = self.get_rabbitmq_memory_usage()
+            remote.check_call(cmd.format(memory=int(mem_usage * 1.0001)))
+            self.check_alarms(alarm_type, source, controller["hostname"],
+                              CRITICAL_STATUS)
+            self.try_set_memory_limit(controller, default_value)
+            self.check_alarms(alarm_type, source, controller["hostname"],
+                              OKAY_STATUS)
+
     @test(depends_on_groups=["deploy_toolchain"],
           groups=["check_mysql_fs_alarms", "toolchain", "alarms"])
     @log_snapshot_after_test
@@ -75,3 +120,53 @@ class TestToolchainAlarms(api.ToolchainApi):
         self._check_filesystem_alarms(
             controller, "/dev/mapper/mysql-root", "mysql-fs",
             "/var/lib/mysql/test/bigfile")
+
+    @test(depends_on_groups=["deploy_toolchain"],
+          groups=["check_rabbitmq_disk_limit_alarm", "toolchain", "alarms"])
+    @log_snapshot_after_test
+    def check_rabbitmq_disk_limit_alarm(self):
+        """Check that rabbitmq-disk-limit-warning and
+        rabbitmq-disk-limit-critical alarms work as expected.
+
+        Scenario:
+            1. Check the last value of the okay alarm in InfluxDB.
+            2. Set RabbitMQ disk limit to 99.99 percent of available space.
+            3. Check the last value of the warning alarm in InfluxDB.
+            4. Set RabbitMQ disk limit to 100 percent of available space.
+            5. Check the last value of the critical alarm in InfluxDB.
+            6. Set RabbitMQ disk limit to the default value.
+            7. Check the last value of the okay alarm in InfluxDB.
+
+        Duration 10m
+        """
+        self.env.revert_snapshot("deploy_toolchain")
+        controller = self.fuel_web.get_nailgun_cluster_nodes_by_roles(
+            self.helpers.cluster_id, ["controller"])[0]
+        self._check_rabbit_mq_alarms(controller, "service", "disk")
+
+    @test(depends_on_groups=["deploy_toolchain"],
+          groups=["check_rabbitmq_memory_watermark_alarm", "toolchain",
+                  "alarms"])
+    @log_snapshot_after_test
+    def check_rabbitmq_memory_watermark_alarm(self):
+        """Check that rabbitmq-memory-limit-warning and
+        rabbitmq-memory-limit-critical alarms work as expected.
+
+        Scenario:
+            1. Check the last value of the okay alarm in InfluxDB.
+            2. Set RabbitMQ memory limit to 101 percent of currently
+            used memory.
+            3. Check the last value of the warning alarm in InfluxDB.
+            4. Set RabbitMQ memory limit to 100.01 percent of currently
+            used memory.
+            5. Check the last value of the critical alarm in InfluxDB.
+            6. Set RabbitMQ memory limit to the default value.
+            7. Check the last value of the okay alarm in InfluxDB.
+
+        Duration 10m
+        """
+        self.env.revert_snapshot("deploy_toolchain")
+        controller = self.fuel_web.get_nailgun_cluster_nodes_by_roles(
+            self.helpers.cluster_id, ["controller"])[0]
+
+        self._check_rabbit_mq_memory_alarms(controller, "service", "memory")
