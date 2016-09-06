891083e84a329d7975c0156ddd1f1522e9a121b6
vgusev <vgusev@mirantis.com>
2016-09-06 14:03:10 +0300

    Add tests for plugin filesystem alarms
    Change-Id: I078dd8ee7f802caa4d2801b203fd75093a689735

--- stacklight_tests/toolchain/test_alarms.py
+++ stacklight_tests/toolchain/test_alarms.py
@@ -18,54 +18,56 @@ from proboscis import test
 
 from stacklight_tests.toolchain import api
 
-OKAY_STATUS = 0
-WARNING_STATUS = 1
-UNKNOWN_STATUS = 2
-CRITICAL_STATUS = 3
-DOWN_STATUS = 4
-
-WARNING_PERCENT = 91
-CRITICAL_PERCENT = 96
-
-RABBITMQ_DISK_WARNING_PERCENT = 99.99
-RABBITMQ_DISK_CRITICAL_PERCENT = 100
-RABBITMQ_MEMORY_WARNING_VALUE = 1.01
-RABBITMQ_MEMORY_CRITICAL_VALUE = 1.0001
-
 
 @test(groups=["plugins"])
 class TestToolchainAlarms(api.ToolchainApi):
     """Class for testing built-in StackLight Collector alarms.
     """
+    OKAY_STATUS = 0
+    WARNING_STATUS = 1
+    UNKNOWN_STATUS = 2
+    CRITICAL_STATUS = 3
+    DOWN_STATUS = 4
+
+    WARNING_PERCENT = 91
+    CRITICAL_PERCENT = 96
+
+    RABBITMQ_DISK_WARNING_PERCENT = 99.99
+    RABBITMQ_DISK_CRITICAL_PERCENT = 100
+    RABBITMQ_MEMORY_WARNING_VALUE = 1.01
+    RABBITMQ_MEMORY_CRITICAL_VALUE = 1.0001
 
     def _check_filesystem_alarms(self, nailgun_node, filesystem, source,
                                  filename, node_role, alarm_type="node"):
+        if node_role == "elasticsearch-nodes":
+            self.WARNING_PERCENT = 81
+            self.CRITICAL_PERCENT = 86
         self.check_alarms(alarm_type, node_role, source,
-                          nailgun_node["hostname"], OKAY_STATUS)
+                          nailgun_node["hostname"], self.OKAY_STATUS)
         with self.fuel_web.get_ssh_for_nailgun_node(nailgun_node) as remote:
             self.remote_ops.fill_up_filesystem(
-                remote, filesystem, WARNING_PERCENT, filename)
+                remote, filesystem, self.WARNING_PERCENT, filename)
             logger.info("Checking {}-warning alarm".format(source))
             self.check_alarms(alarm_type, node_role, source,
-                              nailgun_node["hostname"], WARNING_STATUS)
+                              nailgun_node["hostname"], self.WARNING_STATUS)
             self.remote_ops.clean_filesystem(remote, filename)
-            self.check_alarms(alarm_type, node_role,
-                              source, nailgun_node["hostname"], OKAY_STATUS)
+            self.check_alarms(alarm_type, node_role, source,
+                              nailgun_node["hostname"], self.OKAY_STATUS)
             self.remote_ops.fill_up_filesystem(
-                remote, filesystem, CRITICAL_PERCENT, filename)
+                remote, filesystem, self.CRITICAL_PERCENT, filename)
             logger.info("Checking {}-critical alarm".format(source))
             self.check_alarms(alarm_type, node_role, source,
-                              nailgun_node["hostname"], CRITICAL_STATUS)
+                              nailgun_node["hostname"], self.CRITICAL_STATUS)
             self.remote_ops.clean_filesystem(remote, filename)
             self.check_alarms(alarm_type, node_role, source,
-                              nailgun_node["hostname"], OKAY_STATUS)
+                              nailgun_node["hostname"], self.OKAY_STATUS)
 
     def _check_rabbit_mq_disk_alarms(self, controller, status, percent):
         cmd = ("rabbitmqctl set_disk_free_limit $(df | grep /dev/dm-4 | "
                "awk '{{ printf(\"%.0f\\n\", 1024 * ((($3 + $4) * "
                "{percent} / 100) - $3))}}')")
         self.check_alarms("service", "rabbitmq", "disk",
-                          controller["hostname"], OKAY_STATUS)
+                          controller["hostname"], self.OKAY_STATUS)
         with self.fuel_web.get_ssh_for_nailgun_node(controller) as remote:
             default_value = remote.check_call(
                 "rabbitmqctl environment | grep disk_free_limit | "
@@ -76,12 +78,12 @@ class TestToolchainAlarms(api.ToolchainApi):
             remote.check_call("rabbitmqctl set_disk_free_limit {}".format(
                 default_value))
             self.check_alarms("service", "rabbitmq", "disk",
-                              controller["hostname"], OKAY_STATUS)
+                              controller["hostname"], self.OKAY_STATUS)
 
     def _check_rabbit_mq_memory_alarms(self, controller, status, value):
         cmd = "rabbitmqctl set_vm_memory_high_watermark absolute \"{memory}\""
         self.check_alarms("service", "rabbitmq", "memory",
-                          controller["hostname"], OKAY_STATUS)
+                          controller["hostname"], self.OKAY_STATUS)
         with self.fuel_web.get_ssh_for_nailgun_node(controller) as remote:
             default_value = remote.check_call(
                 "rabbitmqctl environment | grep disk_free_limit | "
@@ -92,7 +94,7 @@ class TestToolchainAlarms(api.ToolchainApi):
                               controller["hostname"], status)
             self.set_rabbitmq_memory_watermark(controller, default_value)
             self.check_alarms("service", "rabbitmq", "memory",
-                              controller["hostname"], OKAY_STATUS)
+                              controller["hostname"], self.OKAY_STATUS)
 
     @test(depends_on_groups=["deploy_toolchain"],
           groups=["check_mysql_fs_alarms", "toolchain", "alarms"])
@@ -141,10 +143,10 @@ class TestToolchainAlarms(api.ToolchainApi):
         self.env.revert_snapshot("deploy_toolchain")
         controller = self.fuel_web.get_nailgun_cluster_nodes_by_roles(
             self.helpers.cluster_id, ["controller"])[0]
-        self._check_rabbit_mq_disk_alarms(controller, WARNING_STATUS,
-                                          RABBITMQ_DISK_WARNING_PERCENT)
-        self._check_rabbit_mq_disk_alarms(controller, CRITICAL_STATUS,
-                                          RABBITMQ_DISK_CRITICAL_PERCENT)
+        self._check_rabbit_mq_disk_alarms(controller, self.WARNING_STATUS,
+                                          self.RABBITMQ_DISK_WARNING_PERCENT)
+        self._check_rabbit_mq_disk_alarms(controller, self.CRITICAL_STATUS,
+                                          self.RABBITMQ_DISK_CRITICAL_PERCENT)
 
     @test(depends_on_groups=["deploy_toolchain"],
           groups=["check_rabbitmq_memory_alarm", "toolchain",
@@ -172,10 +174,11 @@ class TestToolchainAlarms(api.ToolchainApi):
         self.env.revert_snapshot("deploy_toolchain")
         controller = self.fuel_web.get_nailgun_cluster_nodes_by_roles(
             self.helpers.cluster_id, ["controller"])[0]
-        self._check_rabbit_mq_memory_alarms(controller, WARNING_STATUS,
-                                            RABBITMQ_MEMORY_WARNING_VALUE)
-        self._check_rabbit_mq_memory_alarms(controller, CRITICAL_STATUS,
-                                            RABBITMQ_MEMORY_CRITICAL_VALUE)
+        self._check_rabbit_mq_memory_alarms(controller, self.WARNING_STATUS,
+                                            self.RABBITMQ_MEMORY_WARNING_VALUE)
+        self._check_rabbit_mq_memory_alarms(
+            controller, self.CRITICAL_STATUS,
+            self.RABBITMQ_MEMORY_CRITICAL_VALUE)
 
     @test(depends_on_groups=["deploy_ha_toolchain"],
           groups=["check_rabbitmq_pacemaker_alarms", "toolchain", "alarms"])
@@ -207,7 +210,7 @@ class TestToolchainAlarms(api.ToolchainApi):
         self.env.revert_snapshot("deploy_ha_toolchain")
 
         self.check_alarms('service', 'rabbitmq-cluster', 'pacemaker',
-                          None, OKAY_STATUS)
+                          None, self.OKAY_STATUS)
 
         controllers = self.fuel_web.get_nailgun_cluster_nodes_by_roles(
             self.helpers.cluster_id, ["controller"])
@@ -219,11 +222,11 @@ class TestToolchainAlarms(api.ToolchainApi):
             controller_node.name)
         rabbitmq_slaves = self.fuel_web.get_rabbit_slaves_node(
             controller_node.name)
-        ban_and_check_status(rabbitmq_slaves[0], WARNING_STATUS, 120)
-        ban_and_check_status(rabbitmq_slaves[1], CRITICAL_STATUS, 120)
+        ban_and_check_status(rabbitmq_slaves[0], self.WARNING_STATUS, 120)
+        ban_and_check_status(rabbitmq_slaves[1], self.CRITICAL_STATUS, 120)
         # Don't wait for the pcs operation to complete as it will fail since
         # the resource isn't running anywhere
-        ban_and_check_status(rabbitmq_master, DOWN_STATUS)
+        ban_and_check_status(rabbitmq_master, self.DOWN_STATUS)
 
         logger.info("Clear rabbitmq resource")
         with self.fuel_web.get_ssh_for_node(rabbitmq_master.name) as remote:
@@ -231,7 +234,7 @@ class TestToolchainAlarms(api.ToolchainApi):
                                            'master_p_rabbitmq-server',
                                            wait=240)
         self.check_alarms('service', 'rabbitmq-cluster', 'pacemaker',
-                          None, OKAY_STATUS)
+                          None, self.OKAY_STATUS)
 
     @test(depends_on_groups=["deploy_toolchain"],
           groups=["check_root_fs_alarms", "toolchain", "alarms"])
@@ -301,3 +304,51 @@ class TestToolchainAlarms(api.ToolchainApi):
             self.helpers.cluster_id, ["compute"])[0]
         self._check_filesystem_alarms(compute, "/var/lib/nova", "nova-fs",
                                       "/var/lib/nova/bigfile", "compute")
+
+    @test(depends_on_groups=["deploy_toolchain"],
+          groups=["check_influxdb_fs_alarms", "toolchain", "alarms"])
+    @log_snapshot_after_test
+    def check_influxdb_fs_alarms(self):
+        """Check that influxdb-fs-warning and influxdb-fs-critical alarms work
+        as expected.
+
+        Scenario:
+            1. Fill up /var/lib/influxdb filesystem to 91 percent.
+            2. Check the last value of the warning alarm in InfluxDB.
+            3. Clean the filesystem.
+            4. Fill up /var/lib/influxdb filesystem to 96 percent.
+            5. Check the last value of the critical alarm in InfluxDB.
+            6. Clean the filesystem.
+
+        Duration 10m
+        """
+        self.env.revert_snapshot("deploy_toolchain")
+        influxdb_node = self.fuel_web.get_nailgun_cluster_nodes_by_roles(
+            self.helpers.cluster_id, ["influxdb_grafana"])[0]
+        self._check_filesystem_alarms(
+            influxdb_node, "/var/lib/influxdb", "data-fs",
+            "/var/lib/influxdb/bigfile", "influxdb-nodes")
+
+    @test(depends_on_groups=["deploy_toolchain"],
+          groups=["check_elasticsearch_fs_alarms", "toolchain", "alarms"])
+    @log_snapshot_after_test
+    def check_elasticsearch_fs_alarms(self):
+        """Check that elasticsearch-fs-warning and elasticsearch-fs-critical
+        alarms work as expected.
+
+        Scenario:
+            1. Fill up /opt/es-data filesystem to 81 percent.
+            2. Check the last value of the warning alarm in InfluxDB.
+            3. Clean the filesystem.
+            4. Fill up /opt/es-data filesystem to 86 percent.
+            5. Check the last value of the critical alarm in InfluxDB.
+            6. Clean the filesystem.
+
+        Duration 10m
+        """
+        self.env.revert_snapshot("deploy_toolchain")
+        elasticsearch_node = self.fuel_web.get_nailgun_cluster_nodes_by_roles(
+            self.helpers.cluster_id, ["elasticsearch_kibana"])[0]
+        self._check_filesystem_alarms(
+            elasticsearch_node, "/opt/es-data", "data-fs",
+            "/opt/es-data/bigfile", "elasticsearch-nodes")
