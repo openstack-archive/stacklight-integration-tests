378dd92149278a7e6302f99b0edc0b5f5cc5c2ee
Vladimir Ushakov <vushakov@mera.ru>
2016-09-08 09:14:35 +0300

    Add five zabbix functional tests.
    Add test_zabbix_deployment test.
    Add check_zabbix_api test.
    Add check_zabbix_dashboard_configuration test.
    Add test_triggers test.
    Add test_trigger_api test.
    Add related zabbix API methods.
    
    Change-Id: Ibb01f12a4be1492b457c889143784789edc4b742

--- stacklight_tests/run_tests.py
+++ stacklight_tests/run_tests.py
@@ -69,7 +69,7 @@ def import_tests():
     from stacklight_tests.toolchain import test_reduced_footprint  # noqa
     from stacklight_tests.toolchain import test_smoke_bvt  # noqa
     from stacklight_tests.toolchain import test_system  # noqa
-
+    from stacklight_tests.zabbix import test_functional  # noqa
 
 def run_tests():
     from proboscis import TestProgram  # noqa
