44e7df9a7de6c4b008eddb908b2d8cf16e15c1f8
Artem <aminasyan@mirantis.com>
2016-09-15 14:47:00 +0300

    Deploy environment with Telemetry plugin and Kafka plugin enabled
    Change-Id: I96763dfbfe87dae63c1f99b985014957108ac2ef

---
+++ stacklight_tests/toolchain/test_telemetry_kafka.py
@@ -0,0 +1,68 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from fuelweb_test.helpers.decorators import log_snapshot_after_test
+from proboscis import test
+
+from stacklight_tests.kafka import api as kafka_api
+from stacklight_tests.toolchain import api
+
+
+@test(groups=["plugins"])
+class TestOpenstackTelemetryKafka(api.ToolchainApi):
+    """Class for smoke testing the Openstack Telemetry Plugin
+     with Kafka plugin.
+     """
+
+    @test(depends_on_groups=['prepare_slaves_3'],
+          groups=["deploy_openstack_telemetry_kafka", "install",
+                  "ceilometer_redis", "smoke"])
+    @log_snapshot_after_test
+    def deploy_openstack_telemetry_kafka(self):
+        """Deploy an environment with Openstack-Telemetry plugin
+        with Elasticsearch and InfluxDB backends.
+
+            1. Upload the Openstack-Telemetry, Elasticsearch-Kibana, Kafka
+            InfluxDB-Grafana plugins to the master node
+            2. Install the plugins
+            3. Create the cluster
+            4. Add 1 nodes with controller role and kafka
+            5. Add 1 nodes with compute and cinder roles
+            6. Add 1 nodes with elasticsearch_kibana and influxdb_grafana roles
+            7. Deploy the cluster
+            8. Check that plugins are running
+            9. Run OSTF
+
+        Duration 90m
+        """
+        self.check_run("deploy_openstack_telemetry_kafka")
+        self.env.revert_snapshot("ready_with_3_slaves")
+        self.add_plugin(self.OPENSTACK_TELEMETRY)
+        self.add_plugin(kafka_api.KafkaPluginApi())
+        self.disable_plugin(self.LMA_COLLECTOR)
+        self.disable_plugin(self.LMA_INFRASTRUCTURE_ALERTING)
+        self.prepare_plugins()
+        self.helpers.create_cluster(name=self.__class__.__name__)
+        self.activate_plugins()
+        roles = ["elasticsearch_kibana", "influxdb_grafana"]
+        self.helpers.deploy_cluster(
+            {'slave-01': ['controller', 'kafka'],
+             'slave-02': ['compute', 'cinder'],
+             'slave-03': roles})
+        self.disable_plugin(self.OPENSTACK_TELEMETRY)
+        self.check_plugins_online()
+        self.OPENSTACK_TELEMETRY.check_plugin_online(is_kafka_enabled=True)
+        self.helpers.run_ostf()
+        self.env.make_snapshot("deploy_openstack_telemetry_kafka",
+                               is_make=True)
