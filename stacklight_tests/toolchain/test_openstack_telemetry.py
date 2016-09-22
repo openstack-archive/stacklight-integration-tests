dfae6dda6aee2465949e4957996d823b7bb5ed06
Igor Degtiarov <idegtiarov@mirantis.com>
2016-09-22 13:38:42 +0300

    Functional tests for OpenstackTelemetry plugin
    Testing all Ceilometer functionality in OpenstackTelemetry plugin
    when Event and Resource API are available
    
    Change-Id: I1683cea38c7d21e8a9391bda1b354ebe6a1f4bd1

--- stacklight_tests/toolchain/test_openstack_telemetry.py
+++ stacklight_tests/toolchain/test_openstack_telemetry.py
@@ -196,3 +196,62 @@ class TestOpenstackTelemetryFunctional(api.ToolchainApi):
         self.OPENSTACK_TELEMETRY.check_ceilometer_resource_functionality()
         self.env.make_snapshot("openstack_telemetry_event_functional",
                                is_make=True)
+
+    @test(depends_on_groups=['prepare_slaves_5'],
+          groups=["openstack_telemetry_all_functional",
+                  "deploy_openstack_telemetry", "functional"])
+    @log_snapshot_after_test
+    def openstack_telemetry_all_functional(self):
+        """Deploy an environment with Openstack-Telemetry plugin with
+        enabled Ceilometer Event and Resource API and check its functionality
+
+            1. Upload the Openstack-Telemetry, Elasticsearch-Kibana and
+            InfluxDB-Grafana plugins to the master node
+            2. Install the plugins
+            3. Create the cluster
+            4. Add 3 nodes with controller role
+            5. Add 1 nodes with compute and cinder roles
+            6. Add 1 nodes with elasticsearch_kibana and influxdb_grafana roles
+            9. Enable Ceilometer Event and Resource API
+            10. Deploy the cluster
+            11. Check that plugins are running
+            12. Run OSTF
+            13. Check Ceilometer Sample API
+            14. Check Ceilometer Alarm API
+            15. Check Ceilometer Event API
+            16. Check Ceilometer Resource API
+
+        Duration 90m
+        """
+        self.check_run("openstack_telemetry_all_functional")
+        self.env.revert_snapshot("ready_with_5_slaves")
+        self.add_plugin(self.OPENSTACK_TELEMETRY)
+        self.disable_plugin(self.LMA_COLLECTOR)
+        self.disable_plugin(self.LMA_INFRASTRUCTURE_ALERTING)
+        self.prepare_plugins()
+        self.helpers.create_cluster(name=self.__class__.__name__)
+        self.activate_plugins()
+
+        options = {
+            "advanced_settings/value": True,
+            "event_api/value": True,
+            "resource_api/value": True,
+        }
+
+        self.OPENSTACK_TELEMETRY.activate_plugin(options=options)
+
+        roles = ["elasticsearch_kibana", "influxdb_grafana"]
+        self.helpers.deploy_cluster(
+            {'slave-01': ['controller'],
+             'slave-02': ['controller'],
+             'slave-03': ['controller'],
+             'slave-04': ['compute', 'cinder'],
+             'slave-05': roles})
+        self.check_plugins_online()
+        self.helpers.run_ostf()
+        self.OPENSTACK_TELEMETRY.check_ceilometer_sample_functionality()
+        self.OPENSTACK_TELEMETRY.check_ceilometer_alarm_functionality()
+        self.OPENSTACK_TELEMETRY.check_ceilometer_event_functionality()
+        self.OPENSTACK_TELEMETRY.check_ceilometer_resource_functionality()
+        self.env.make_snapshot("openstack_telemetry_all_functional",
+                               is_make=True)
