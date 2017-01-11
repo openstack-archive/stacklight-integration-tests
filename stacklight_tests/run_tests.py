6f4f7661832fe2656e91b118b0ac767fdf69b15c
Olivier Bourdon <obourdon@mirantis.com>
2017-01-11 14:05:37 +0100

    Add regression test suite for Zabbix plugin
    First bug to enter regression test case:
    https://bugs.launchpad.net/fuel/+bug/1633701
    [zabbix plugin] Error when deploying on MOS 9.0 env with modified hostnames
    
    Change-Id: I284f12363738b0769b51d698012960bca6f9e8fd

--- stacklight_tests/run_tests.py
+++ stacklight_tests/run_tests.py
@@ -74,6 +74,7 @@ def import_tests():
     from stacklight_tests.zabbix import test_functional  # noqa
     from stacklight_tests.zabbix import test_smoke_bvt  # noqa
     from stacklight_tests.zabbix import test_system  # noqa
+    from stacklight_tests.zabbix import test_regression  # noqa
 
 
 def run_tests():
