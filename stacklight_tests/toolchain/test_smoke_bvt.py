06e62c7fde08ba25a08a7c7b671fd83e1ce6397a
Olivier Bourdon <obourdon@mirantis.com>
2017-01-15 19:35:37 +0100

    Add test for Nova aggregates
    Change-Id: Ic1f48c74ba45fdb9e3c7a2368c5e73b34b371bd3

--- stacklight_tests/toolchain/test_smoke_bvt.py
+++ stacklight_tests/toolchain/test_smoke_bvt.py
@@ -13,6 +13,7 @@
 #    under the License.
 
 from fuelweb_test.helpers.decorators import log_snapshot_after_test
+from proboscis import asserts
 from proboscis import test
 
 from stacklight_tests.toolchain import api
@@ -80,6 +81,63 @@ class TestToolchain(api.ToolchainApi):
         self.helpers.run_ostf()
 
         self.env.make_snapshot("deploy_toolchain", is_make=True)
+
+    @test(depends_on_groups=['prepare_slaves_9'],
+          groups=["deploy_nova_toolchain", "deploy", "toolchain", "smoke"])
+    @log_snapshot_after_test
+    def deploy_nova_toolchain(self):
+        """Deploy a nova cluster with the LMA Toolchain plugins
+
+        Scenario:
+            1. Upload the LMA Toolchain plugins to the master node
+            2. Install the plugins
+            3. Create the cluster
+            4. Add 1 node with controller and cinder role
+            5. Add 4 node with compute
+            6. Add 1 node with plugin roles
+            7. Deploy the cluster
+            8. Create 2 nova aggregates with respectively 2+1 hosts
+            9. Check that LMA Toolchain plugins are running
+            10. Run OSTF
+
+        Duration 60m
+        Snapshot deploy_nova_toolchain
+        """
+        self.check_run("deploy_nova_toolchain")
+        self.env.revert_snapshot("ready_with_9_slaves")
+
+        self.prepare_plugins()
+
+        self.helpers.create_cluster(name=self.__class__.__name__)
+
+        self.activate_plugins()
+
+        self.helpers.deploy_cluster(self.settings.nova_nodes)
+
+        # Get all nodes names with compute role
+        compute_nodes_names = map(
+            lambda(l): l[0],
+            filter(
+                lambda(name,roles): any('compute' in s for s in roles),
+                self.settings.nova_nodes.iteritems()))
+        # Check that there are enough compute nodes for proper testing
+        asserts.assert_true(
+            len(compute_nodes_names) > 3,
+            "Not enough compute nodes {0} (must be > 3)".format(
+                len(compute_nodes_names)))
+        # Sort list
+        compute_nodes_names.sort()
+        # Create nova aggregates 1st with 2 nodes second with 1 node
+        # (this leaves 1 compute node not assigned to ny aggregate)
+        self.helpers.create_nova_aggregates(
+            { "aggregate-1": compute_nodes_names[0:2], 
+              "aggregate-2": compute_nodes_names[2:3]})
+
+        self.check_plugins_online()
+
+        self.helpers.run_ostf()
+
+        self.env.make_snapshot("deploy_nova_toolchain", is_make=True)
 
     @test(depends_on_groups=['prepare_slaves_9'],
           groups=["deploy_ha_toolchain", "deploy", "deploy_ha", "toolchain",
